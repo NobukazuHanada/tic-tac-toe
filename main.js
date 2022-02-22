dc3e319f89961d9d6dea3f03c01ecbcd810d8450
nobkz <tone.nobukazu@gmail.com>
2022-02-22 19:31:34 +0900

    ○の入力ができるようにした

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const TicTacToeTable = document.getElementById("tic-tac-toe-table");
+
+TicTacToeTable.addEventListener("click", (e)=>{
+    console.log(`user clicked!`);
+    const maru = document.createTextNode("○");
+    e.target.appendChild(maru);
+});
